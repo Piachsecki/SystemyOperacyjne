a6b219c37fbbbe32b8d0209a306e458dcc900ef2
Piasecki-grid <kpiasecki@griddynamics.com>
2025-03-29 19:17:37 +0100

    Basic logic implementation of a program

--- main.py
+++ main.py
@@ -1,21 +1,71 @@
 import logging
 import threading
 import time
+import sys
+import random
 
-def thread_function(name):
-    logging.info("Thread %s: starting", name)
-    time.sleep(2)
-    logging.info("Thread %s: finishing", name)
+number_of_philosophers = sys.argv[0]
+
+class Philosopher:
+    def __init__(self, id, his_left_fork, his_right_fork):
+        self.id = id
+        self.his_left_fork = his_left_fork
+        self.his_right_fork = his_right_fork
+    
+    def thinking(self):
+        time.sleep(random.uniform(1, 5))
+        logging("Philosopher %d is thinking", id)
+    
+    def eating(self):
+        logging("Philosopher %d is eating", id)
+        time.sleep(random.uniform(1, 5))
+
+
+    def pick_up_forks(self):
+        if not self.his_left_fork.locked() and not self.his_right_fork.locked():
+            logging(f"Philosopher can try picking forks, because both of his left and right are available!")
+        
+        if self.id % 2 == 0:
+            self.his_left_fork.acquire()
+            logging(f"Philosopher {self.id} is picking up his left fork")
+            time.sleep(random.uniform(1, 2))
+
+            self.his_right_fork.acquire()
+            logging(f"Philosopher {self.id} is picking up his left fork")
+            time.sleep(random.uniform(1, 2))
+               
+            
+        else:
+            self.his_right_fork.acquire()
+            logging(f"Philosopher {self.id} is picking up his right fork")
+            time.sleep(random.uniform(1, 2))
+
+            self.his_left_fork.acquire()
+            logging(f"Philosopher {self.id} is picking up his left fork")
+            time.sleep(random.uniform(1, 2))  
+
+        logging("Philosopher %d picked up forks", id)   
+
+    def put_back_forks(self):
+        logging("Philosopher %d is putting back forks", id)
+        self.his_left_fork.release()
+        self.his_right_fork.release()
+
 
 if __name__ == "__main__":
-    format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format, level=logging.INFO,
-                        datefmt="%H:%M:%S")
+    print(f"Executing script: {sys.argv[0]}")
 
-    logging.info("Main    : before creating thread")
-    x = threading.Thread(target=thread_function, args=(1,))
-    logging.info("Main    : before running thread")
-    x.start()
-    logging.info("Main    : wait for the thread to finish")
-    # x.join()
-    logging.info("Main    : all done")
+    if len(sys.argv) < 2:
+        print("You must pass number of philosophers!")
+        exit
+    forks = []
+    philosophers = []
+
+    for philosopher in range(number_of_philosophers):
+        forks.append(threading.Lock())
+    
+    for philosopher in range(number_of_philosophers):
+        philosophers.append(Philosopher(philosopher, forks[philosopher], forks[(philosopher + 1) % number_of_philosophers]))
+
+    print("Hello")
+
